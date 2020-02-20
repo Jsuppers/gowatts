eff846d0533f18307be236b804ce97b4ffc2b0f5
Jsuppers <jorissuppers@gmail.com>
2020-02-20 12:09:04 +0100

    remove unneeded parameters

--- data/parameters.go
+++ data/parameters.go
@@ -2,5 +2,5 @@ package data
 
 // Parameters holds the input for the API API call
 type Parameters struct {
-	APIKey, Timeframe, Tilt, Losses, Azimuth, Latitude, Longitude, Capacity, ArrayType, ModuleType, Zoom string
+	Tilt, Losses, Azimuth, Latitude, Longitude, Capacity, ArrayType, ModuleType, Zoom string
 }
